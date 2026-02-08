126e3978e5c3441be0e4f676ba469892a60e03c8
hellowmarketing2-prog <hellow.marketing.2@gmail.com>
2026-02-08 09:37:20 +0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ✅ Only one BrowserRouter here
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // ✅ change here
 import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>   {/* ✅ change here */}
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // base: "/bot-builders--branders/",   // 👈 yahan repo ka exact naam
+  base: "/developers/",   // 👈 yahan repo ka exact naam
 })
